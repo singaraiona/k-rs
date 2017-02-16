08889054fb5c290adeecce5625eb70cbb2541eb7
singaraiona <singaraiona@gmail.com>
2017-02-16 18:14:11 +0200

    started

--- src/app/repl.rs
+++ src/app/repl.rs
@@ -17,6 +17,13 @@ fn main() {
     let mut p = parser::new();
     let mut i = Interpreter::new();
     let mut input = vec![0u8; 256];
+
+    let v1 = [1, 2, 3, 4];
+    let v2 = 7;
+
+    let r = v1.iter().map(|x| x + v2);
+    println!("R: {:?}", r);
+
     println!("K\\ {}", VERSION);
     ps1();
     loop {
